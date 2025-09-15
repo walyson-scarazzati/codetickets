7a60e69865cfc197e420eb4c4237fe828b36b86a
Walyson Scarazzati da Silva <53382989+walyson-scarazzati@users.noreply.github.com>
2025-09-15 17:53:58 +0100

    03. Lendo e escrevendo dados em lote

--- src/main/java/br/com/alura/codetickets/ImportacaoJobConfiguration.java
+++ src/main/java/br/com/alura/codetickets/ImportacaoJobConfiguration.java
@@ -9,12 +9,17 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.time.LocalDateTime;
 
 @Configuration
 public class ImportacaoJobConfiguration {
@@ -46,8 +51,22 @@ public class ImportacaoJobConfiguration {
                 .resource(new FileSystemResource("files/dados.csv"))
                 .comments("--")
                 .delimited()
+                .delimiter(";")
                 .names("cpf", "cliente", "nascimento", "evento", "data", "tipoIngresso", "valor")
-                .targetType(Importacao.class)
+                .fieldSetMapper(new ImportacaoMapper())
+                .build();
+    }
+
+    @Bean
+    public ItemWriter<Importacao> writer(DataSource dataSource) {
+        return new JdbcBatchItemWriterBuilder<Importacao>()
+                .dataSource(dataSource)
+                .sql(
+                        "INSERT INTO importacao (cpf, cliente, nascimento, evento, data, tipo_ingresso, valor, hora_importacao) VALUES" +
+                                " (:cpf, :cliente, :nascimento, :evento, :data, :tipoIngresso, :valor, :horaImportacao)"
+
+                )
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
                 .build();
     }
 
--- src/main/java/br/com/alura/codetickets/modelo/Importacao.java
+++ src/main/java/br/com/alura/codetickets/modelo/Importacao.java
@@ -1,10 +1,6 @@
 package br.com.alura.codetickets.modelo;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
