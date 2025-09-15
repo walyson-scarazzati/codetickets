3aef1952c700efd1fa34a80c9c5cc513884dbe53
Walyson Scarazzati da Silva <53382989+walyson-scarazzati@users.noreply.github.com>
2025-09-15 07:41:47 +0100

    01. Entendendo o processamento em lote

---
+++ src/main/java/br/com/alura/codetickets/modelo/Importacao.java
@@ -0,0 +1,100 @@
+package br.com.alura.codetickets.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+public class Importacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String cpf;
+    private String cliente;
+    private LocalDate nascimento;
+    private String evento;
+    private LocalDate data;
+    private String tipoIngresso;
+    private Double valor;
+    private LocalDateTime horaImportacao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public LocalDate getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(LocalDate nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getEvento() {
+        return evento;
+    }
+
+    public void setEvento(String evento) {
+        this.evento = evento;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String getTipoIngresso() {
+        return tipoIngresso;
+    }
+
+    public void setTipoIngresso(String tipoIngresso) {
+        this.tipoIngresso = tipoIngresso;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public LocalDateTime getHoraImportacao() {
+        return horaImportacao;
+    }
+
+    public void setHoraImportacao(LocalDateTime horaImportacao) {
+        this.horaImportacao = horaImportacao;
+    }
+
+
+}
---
+++ src/test/java/br/com/alura/codetickets/CodeticketsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.codetickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeticketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
